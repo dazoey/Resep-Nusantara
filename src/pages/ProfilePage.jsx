41d0682bc0d6d01c960350a1ad2a1097397dacd5
dazoey <151373427+dazoey@users.noreply.github.com>
2025-10-26 03:26:01 +0700

    tambah identitas kelompok

--- src/pages/ProfilePage.jsx
+++ src/pages/ProfilePage.jsx
@@ -8,7 +8,22 @@ export default function ProfilePage() {
         </h1>
         <div className="bg-white rounded-lg shadow-lg p-6">
           <p className="text-gray-600">
-            Konten halaman profile akan diisi di sini...
+            Kelompok 10
+          </p>
+          <p className="text-gray-600">
+            Shift: 2
+          </p>
+          <p className="text-gray-600">
+            Muhammad Rifky Athaya: 21120123140129
+          </p>
+          <p className="text-gray-600">
+            Muhammad Fakhri Akmal: 21120123140147
+          </p>
+          <p className="text-gray-600">
+            Razzaq Permana: 21120123120016
+          </p>
+          <p className="text-gray-600">
+            Fadhlan Yuqa T: 21120123140147
           </p>
         </div>
       </div>
